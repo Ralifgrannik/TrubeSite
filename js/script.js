335be52e06b837e72e243b0e78c5ed489b40f195
Ralif <vafinralif@mail.ru>
2024-11-01 14:20:06 +0300

    add section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Функция для загрузки HTML-файла и замены содержимого
 function loadTheory(fileName) {
-    console.log("Загрузка файла:", fileName); // Эта строка поможет с отладкой
+    console.log("Загрузка файла:", fileName);
     fetch(fileName)
         .then(response => {
             if (!response.ok) {
@@ -10,14 +10,18 @@ function loadTheory(fileName) {
         })
         .then(data => {
             document.getElementById("theory-content").innerHTML = data;
+            // Установите обработчики для новых ссылок после загрузки
+            setupLinkHandlers();
         })
         .catch(error => {
             console.error("Ошибка:", error);
         });
 }
 
+
 // Привязываем функцию к каждому изображению
 document.querySelectorAll(".gallery img").forEach(img => {
+    console.log('jello')
     img.addEventListener("click", () => {
         let fileName = "";
 
@@ -26,7 +30,7 @@ document.querySelectorAll(".gallery img").forEach(img => {
                 fileName = "mechanics.html";
                 break;
             case "Термодинамика и молекулярная физика":
-                fileName = "thermodynamics.html";
+                fileName = "Mechanics/1_chapter.html";
                 break;
             case "Электростатика и электродинамика":
                 fileName = "electrostatics.html";
@@ -49,3 +53,82 @@ document.querySelectorAll(".gallery img").forEach(img => {
         }
     });
 });
+
+
+function setupLinkHandlers() {
+    // Привязываем функцию к каждому элементу с классом mechanics__chapter
+    document.querySelectorAll(".mechanics__chapter").forEach(el => {
+        el.addEventListener("click", (event) => {
+            event.preventDefault(); // Предотвращаем переход по ссылке
+            let fileName = "";
+
+            // Определяем файл в зависимости от ID элемента
+            switch (el.id) {
+                case "1":
+                    fileName = "Mechanics/1_chapter.html";
+                    break;
+                case "2":
+                    fileName = "Mechanics/2_chapter.html";
+                    break;
+                case "3":
+                    fileName = "Mechanics/3_chapter.html";
+                    break;
+                case "4":
+                    fileName = "Mechanics/4_chapter.html";
+                    break;
+                case "5":
+                    fileName = "Mechanics/5_chapter.html";
+                    break;
+                case "6":
+                    fileName = "Mechanics/6_chapter.html";
+                    break;
+                case "7":
+                    fileName = "Mechanics/7_chapter.html";
+                    break;
+                case "8":
+                    fileName = "Mechanics/8_chapter.html";
+                    break;
+                case "9":
+                    fileName = "Mechanics/9_chapter.html";
+                    break;
+                case "10":
+                    fileName = "Mechanics/10_chapter.html";
+                    break;
+                default:
+                    fileName = "";
+            }
+
+            if (fileName) {
+                loadTheory(fileName); // Загружаем теорию
+            }
+        });
+    });
+
+
+    document.querySelectorAll(".a__back").forEach(el => {
+        el.addEventListener("click", (event) => {
+            event.preventDefault(); // Предотвращаем переход по ссылке
+            let fileName = "";
+
+            // Определяем файл в зависимости от ID элемента
+            switch (el.id) {
+                case "1":
+                    fileName = "mechanics.html";
+                    break;
+                case "2":
+                    fileName = "";
+                    break;
+                default:
+                    fileName = "";
+                
+            }
+
+            if (fileName) {
+                loadTheory(fileName); // Загружаем теорию
+            }
+
+        });
+    });
+}
+
+
